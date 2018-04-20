d49e7e3c5dd7b3a5130ba72acea0248ad962d833
DoLikeWater <1014549767@qq.com>
2018-04-21 00:17:25 +0800

    Merge branch 'master' of https://github.com/MoYan-Desolate/jiucengyaota

---
+++ DemonTower/Assets/DungeonCharacters/Scripts/newmove.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class newmove : MonoBehaviour
+{
+
+    private Transform camTrans;
+    private Vector3 camAng;
+    private float camHeight = 2.2f;
+    private float comback = -1f;
+    // Use this for initialization
+    void Start()
+    {
+        Cursor.visible = false;
+        camTrans = Camera.main.transform;
+        Vector3 startPos = transform.position;
+        startPos.y += camHeight;
+        startPos.z += comback;
+        camTrans.position = startPos;
+        camTrans.rotation = transform.rotation;
+        camAng = camTrans.eulerAngles;
+        camTrans.LookAt(this.transform.position);
+    }
+
+    void Rot_move()
+    {
+        float y = Input.GetAxis("Mouse X");
+        float x = Input.GetAxis("Mouse Y");
+        camAng.x -= x;
+        camAng.y += y;
+        camTrans.eulerAngles = camAng;
+        camTrans.position = new Vector3(this.transform.position.x, camTrans.position.y, this.transform.position.z);
+        float camy = camAng.y;
+        this.transform.eulerAngles = new Vector3(this.transform.eulerAngles.x, camy, this.transform.eulerAngles.z);
+        Vector3 startPos = transform.position;
+        startPos.y += camHeight;
+        startPos.z += comback;
+        camTrans.position = startPos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rot_move();
+        if (Input.GetKey(KeyCode.W))
+        {
+            this.transform.Translate(Vector3.forward * Time.deltaTime * 2f);
+        }
+    }
+}
