eff71fcc66e5f57c5e9dcaa9c6e2555d3b69d1f2
DoLikeWater <1014549767@qq.com>
2018-04-20 23:32:56 +0800

    场景脚本的添加

---
+++ DemonTower/Assets/script/pickup.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pickup : MonoBehaviour {
+
+	public GameObject people;
+	public int flag;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if( ( (people.transform.position.x - this.transform.position.x)*(people.transform.position.x - this.transform.position.x) 
+			+ (people.transform.position.z - this.transform.position.z)*(people.transform.position.z - this.transform.position.z) )
+		    <= 1.0f
+		){
+			
+			if (Input.GetKeyDown (KeyCode.F)) {
+
+				switch(flag){
+				case 0:
+					break;
+				case 1:
+					break;
+				default:
+					break;
+				}
+
+				Destroy(this.gameObject,1);  
+			} 
+		}
+	}
+}
