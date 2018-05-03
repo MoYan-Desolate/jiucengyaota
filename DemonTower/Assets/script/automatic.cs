7b07a86593fc4b23e5fed4f1ff56bb5983e7e861
DoLikeWater <1014549767@qq.com>
2018-05-03 13:23:37 +0800

    重新上传

---
+++ DemonTower/Assets/script/automatic.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class automatic : MonoBehaviour {
+
+	public GameObject people;
+	public GameObject plant;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (plant) {
+
+			if (this.transform.position.z - people.transform.position.z >= 0) {
+
+				if (((people.transform.position.x - this.transform.position.x) * (people.transform.position.x - this.transform.position.x)
+				    + (people.transform.position.z - this.transform.position.z) * (people.transform.position.z - this.transform.position.z))
+				    <= 2.0f) {
+
+
+					if (this.transform.position.y < 6) {
+
+						this.transform.Translate (0, 1 * Time.deltaTime, 0);
+					}
+				} else {
+
+					if (this.transform.position.y > 2) {
+
+						this.transform.Translate (0, -1 * Time.deltaTime, 0);
+					}
+				}
+			} else {
+
+				if (this.transform.position.y > 2) {
+
+					this.transform.Translate (0, -1 * Time.deltaTime, 0);
+				}
+			}
+		} else {
+
+			if (((people.transform.position.x - this.transform.position.x) * (people.transform.position.x - this.transform.position.x)
+			    + (people.transform.position.z - this.transform.position.z) * (people.transform.position.z - this.transform.position.z))
+			    <= 2.0f) {
+
+				if (this.transform.position.y < 6) {
+
+					this.transform.Translate (0, 1 * Time.deltaTime, 0);
+				}
+			} else {
+
+				if (this.transform.position.y > 2) {
+
+					this.transform.Translate (0, -1 * Time.deltaTime, 0);
+				}
+			}
+		}
+
+	}
+}
---
+++ DemonTower/Assets/script/pickup.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pickup : MonoBehaviour {
+
+	public GameObject people;
+	public GameObject thing;
+	public int flag;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if( ( (people.transform.position.x - this.transform.position.x)*(people.transform.position.x - this.transform.position.x) 
+			+ (people.transform.position.z - this.transform.position.z)*(people.transform.position.z - this.transform.position.z) )
+		    <= 1.0f
+		){
+			
+			if (Input.GetKeyDown (KeyCode.F)) {
+
+				switch(flag){
+				case 0:
+					create ();
+					break;
+				case 1:
+					break;
+				default:
+					break;
+				}
+
+				Destroy(this.gameObject,1);  
+			} 
+		}
+	}
+
+	void create(){
+
+		thing.transform.position = this.transform.position;
+	}
+}
